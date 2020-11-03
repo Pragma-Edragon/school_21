77297c77b4f5dad14875627d6ec229e33dee97da
r1v3n <rivenfornotifications@mail.ru>
2020-11-03 13:27:21 +0300

    putstr implementation

---
+++ libft_riven/ft_putstr_fd.c
@@ -0,0 +1,16 @@
+// TODO HEADERS!!!
+
+#include <fcntl.h>
+
+void ft_putstr_fd(char *s, int fd)
+{
+    if (!s || !*s)
+        return ;
+    if (fd < 0)
+        return ;
+    while (*s)
+    {
+        ft_putchar_fd(*s, fd);
+        s++;
+    }
+}
