6f582bd67d1e5103ac3574ed14d66d997de1cb87
r1v3n <rivenfornotifications@mail.ru>
2020-11-03 13:42:57 +0300

    putnbr implementation

---
+++ libft_riven/ft_putnbr_fd.c
@@ -0,0 +1,16 @@
+// TODO HEADER!!!
+
+#include <fcntl.h>
+
+void ft_putnbr_fd(int n, int fd)
+{
+    char *number;
+    long int cp;
+
+    if (fd < 0)
+        return ;
+    cp = (long int)n;
+    number = ft_itoa(cp);
+    ft_putstr_fd(number, fd);
+    free(number);
+}
