c6f682a0ca70a98337eb3f36011734af0504ad8c
r1v3n <rivenfornotifications@mail.ru>
2020-11-04 16:32:59 +0300

    fix

--- libft_riven/ft_calloc.c
+++ libft_riven/ft_calloc.c
@@ -5,15 +5,9 @@
 void *ft_calloc(size_t items, size_t size)
 {
     void *pointer;
-    size_t iter;
 
-    iter = 0;
-    pointer = items == 0 || size == 0 ? (void *)0 : malloc(items * size);
-    if (pointer)
-    {
-        while (iter < items * size)
-            *(unsigned char *)(pointer + iter++) = '\0';
-        return ((void *)pointer);
-    }
+    if (!(pointer = malloc(items * size)))
+        return ((void *)0);
+    ft_bzero(pointer, (items * size));
     return (pointer);
 }
