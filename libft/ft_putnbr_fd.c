8cffddbdbce82aaccbaad76367fbbd1f9b87efa0
R1V3N <45512613+Pragma-Edragon@users.noreply.github.com>
2020-11-09 20:34:15 +0300

    Update ft_putnbr_fd.c

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -17,11 +17,14 @@ void			ft_putnbr_fd(int n, int fd)
 	char		letter;
 	long long	cp;
 
+
+	if (fd < 0)
+		return ;	
 	cp = (long long)n;
 	if (cp < 0)
 	{
 		cp *= -1;
-		write(1, "-", 1);
+		write(fd, "-", 1);
 	}
 	if (cp / 10)
 		ft_putnbr_fd(cp / 10, fd);
